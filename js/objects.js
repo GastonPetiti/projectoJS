ce932052e208456412e3ffa7c6b7c475ac2ce5b6
GastonPetiti <sgastonpetiti@gmail.com>
2022-03-14 23:02:18 -0300

    eventos

--- js/objects.js
+++ js/objects.js
@@ -1,16 +1,33 @@
-//creando objetos de los productos que estaran a la venta
 class Botines{
     constructor(marca,modelo,precio){
         this.marca = marca;
         this.mmodelo = modelo;
-        this.precio = precio;
+        this.precio = parseInt(precio);
     }
 }
+ let = listaBotines = [];
 
-const botinesAdidas = new Botines ('Adidas', 'Predator', 30000)
-const botinesNike = new Botines ('Nike', 'Mercurial', 35000)
-const botinesPuma = new Botines ('Puma', 'Future', 25000)
-const botinesNewBalance = new Botines ('New Balance', 'Tekela', 20000)
-// mostrando por consola los productos que estan disponibles
-const productos = [botinesAdidas,botinesNewBalance,botinesNike,botinesPuma]
-console.log (`nuestros productos disponibles son ${productos}`);
+listaBotines.push(new Botines ('Adidas', 'Predator', 40000));
+listaBotines.push(new Botines ('Nike', 'Mercurial', 42000));
+listaBotines.push(new Botines ('Puma', 'Future', 30000));
+listaBotines.push(new Botines ('New Balance', 'Tekela', 22000));
+listaBotines.push(new Botines ('Adidas', 'X', 38000));
+listaBotines.push(new Botines('Adidas','Copa', 33000));
+listaBotines.push(new Botines('Nike','Tiempo',30000));
+listaBotines.push(new Botines('Nike', 'Phantom', 35000));
+listaBotines.push(new Botines('Puma','Ultra',32000));
+listaBotines.push(new Botines('New Balance','Furon',25000));
+listaBotines.push(new Botines('Puma','King',20000));
+
+
+//obtengo la marca y modelo 
+let tipoBotin = document.getElementsByClassName('infoBotin')
+
+//obtengo el precio
+let valorBotin = document.getElementsByClassName('precioBotin')
+
+//mostrando por consola
+const boton = document.addEventListener('click', enviarProducto)
+function enviarProducto (){
+    console.log(`el precio de ${tipoBotin.innerHtml} es de ${valorBotin.value} pesos`)
+}
